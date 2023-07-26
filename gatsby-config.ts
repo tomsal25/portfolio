d93fb09538f0ed2caf609442883cb2b3ca4b7666
tomsal25 <113028016+tomsal25@users.noreply.github.com>
2023-07-27 04:56:55 +0900

    force trailing slash

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,6 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  trailingSlash: `never`,
   siteMetadata: {
     title: "portfolio",
     siteUrl: "https://www.yourdomain.tld",
